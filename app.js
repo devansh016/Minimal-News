3e160fc589872c3b87ab7073ad3a9064b311cf2d
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-14 19:26:02 +0530

    Feedback API fixed

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
-
 //To Get FeedBack File
 app.use('/admin/feedback', (req,res,next) => {
     res.sendFile(path.join(__dirname, 'res/feedback.txt'));
@@ -23,47 +22,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Feedback API
 app.use('/api/feedback', (req,res,next) => {
-    var fb = JSON.stringify(req.body);
-    fs.appendFileSync('res/feedback.txt',`\n ${c_date}: ${fb}`)
-
-    //Logs Updated
-    console.log(fb);
-    
     res.status = 200;
     res.redirect('/');
     return res.send();
 });
 
-//To Send Feedback File
-app.use('/feedback', (req,res,next) => {
-    res.sendFile(path.join(__dirname, "/" + "views" + "/" + "feedback.html"));
-});
-
 //Refresh Data
 app.use('/refresh', (req,res,next) => {
-
     news.news_get();
     res.status =200;
     res.send('refresh done');
-
-    //Logs Updated
-    console.log("Refresh Done.");
 });
-
 
 app.use('/', (req,res,next) => {
     res.sendFile(path.join(__dirname, "/" + "views" + "/" + "index.html"));
 });
 
 var j = schedule.scheduleJob('1 1 * * * *', function() { //run every hour at minute 1 and 1 sec
-
     //Logs Updated
     console.log("Index File Updation Request Send." );
-
     news.news_get(); 
 });
 
 app.listen(port);
 console.log(`Listening at port ${port}`);
+
 //Updating Data First Launch
 news.news_get();
