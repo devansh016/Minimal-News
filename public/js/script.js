1eb66269462496110bcec75bf26d3bd5f9baa5b8
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-06-30 12:44:41 +0530

    dark mode preference saved

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,35 @@ function changeTheme(){
         setCookie('theme','dark',30);
     }
 }
+
+//Checks the theme cookie whether to set dark theme or not
+if(getCookie('theme')=='dark'){
+    trans();
+    document.documentElement.setAttribute('data-theme', 'dark');
+    setCookie('theme','dark',30);
+}
+
+//Function to Create Cookie
+function setCookie(name,value,exp_days) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exp_days*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Function to get Cookies
+function getCookie(name) {
+    var cname = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(cname) == 0){  
+          return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
