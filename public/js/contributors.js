26a7a6e0925dca8194f9fad4315d04a32b7f1764
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-10-02 15:11:20 +0530

    Add Contributors Page

---
+++ public/js/contributors.js
@@ -0,0 +1,17 @@
+$(function () {
+  $.getJSON("/json/contributors", function (data) {
+    console.log(data);
+    $.each(data.contributors, function (i, contributor) {
+      var contributorCol = `
+      <div class="contributor">
+          <div class="contributor_img">
+            <img src="${contributor.image_url}" alt="${contributor.name}" />
+          </div>
+          <h3>${contributor.name}</h3>
+          <a href="${contributor.github_url}" target="_blank"><button class="button"><i class="fab fa-github"></i> Follow</button></a>
+        </div>
+        `;
+      $(contributorCol).appendTo(".contributors");
+    });
+  });
+});
